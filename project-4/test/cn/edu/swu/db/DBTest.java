72daa8ced69ee2784cae84f82df481c3d862b5fb
blank <11570180+smallblank@user.noreply.gitee.com>
2022-11-17 12:57:19 +0800

    实现修改和管理员登录

---
+++ project-4/test/cn/edu/swu/db/DBTest.java
@@ -0,0 +1,33 @@
+package cn.edu.swu.db;
+
+import cn.edu.swu.book.Book;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DBTest {
+
+    public static void main(String[] args) throws SQLException {
+        DBEngine engine = DBEngine.getInstance();
+        String sql = "SELECT `id`, `name`, `author`, `price`, `describe` from book";
+        List<Book> books = engine.query(sql, new RecordVisitor<Book>() {
+            @Override
+            public Book visit(ResultSet rs) throws SQLException {
+                Book book = new Book();
+                book.setId(rs.getLong("id"));
+                book.setName(rs.getString("name"));
+                book.setAuthor(rs.getString("author"));
+                book.setPrice(rs.getFloat("price"));
+                book.setDescribe(rs.getString("describe"));
+                return book;
+            }
+        });
+
+        for (Book book : books) {
+            System.out.println(String.format("%s,%s,%s,%s,%s",
+                    book.getId(), book.getName(), book.getAuthor(), book.getPrice(), book.getDescribe()
+            ));
+        }
+    }
+}
